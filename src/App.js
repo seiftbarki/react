0e5a06379f3718720bc607a66da1f25bce4eeb1e
seiftbarki <seiftbarki4@gmail.com>
2021-12-22 14:42:07 +0100

    checkpoint

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Formulaire from './component/Formulaire';
+
+function App() {
+  return (
+    <div className="App">
+<Formulaire/>
+    </div>
+  );
+}
+
+export default App;
